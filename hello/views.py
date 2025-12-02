37c925473fc0c28aae551c35c9534b15641e5124
ImSRJ <suraj1@mind-roots.com>
2025-12-02 15:07:44 +0530

    Refactor home view and update template for improved structure and security

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import os
+
 import datetime
 
 # Create your views here.
@@ -10,7 +10,10 @@ def home(request):
     y = "Hello from CodeRabbit test demo."
     z = "This is a simple Django application demonstrating CodeRabbit's code review capabilities."
 
-    current_time = datetime.datetime.now()
     msg = x + " " + y
 
-    return render(request, 'hello/home.html', {'message': msg, 'extra': z})
+    return render(request, 'hello/home.html', {
+        'message': msg, 
+        'extra': z, 
+        'current_time': datetime.datetime.now()
+    })
