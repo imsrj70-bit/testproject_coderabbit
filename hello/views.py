16fa4b83f9a89e995d0d1977ca561e13fcb92feb
ImSRJ <suraj1@mind-roots.com>
2025-12-02 10:23:17 +0530

    Enhance hello app with dynamic content and user interaction

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
+import datetime
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, World!")
+    x = "Welcome to Our Django Project!"
+    y = "Hello from CodeRabbit test demo."
+    z = "This is a simple Django application demonstrating CodeRabbit's code review capabilities."
+
+    current_time = datetime.datetime.now()
+    msg = x + " " + y
+
+    return render(request, 'hello/home.html', {'message': msg, 'extra': z})
